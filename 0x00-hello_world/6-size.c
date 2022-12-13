453e03f96a2fda02c8744d9cbace7d88de0108db
kwame7 <kofiagyeman540@gmail.com>
2022-12-12 21:49:38 -0800

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main -Entry
  * Return: Always 0
  */
-
 main(void)
 int main(void)
 {
@@ -12,8 +11,6 @@ printf("Size of an int: %i byte(s)\n",sizeof(int));
 printf("Size of a long int: %i byte(s)\n",sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n",sizeof(long long int));
 printf("Size of a float: %i byte(s)\n",sizeof(float));
-printf("Size of a long long int: %i byte(s)\n",sizeof(long long int)); 
-printf("Size of a float: %i byte(s)\n",sizeof(long int);
-return (0);
-
+printf("Size of a long long int: %i byte(s)\n",sizeof(long long int));
+return 0;
 }
