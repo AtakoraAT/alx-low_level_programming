fa84ba2380a9744e1f97d23964585195b2e4233b
kwame7 <kofiagyeman540@gmail.com>
2022-12-13 02:44:24 -0800

    ama

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -prints the alpha in lowercase
+ */
+
+void print_alphabets(void)
+{
+	char ba;
+
+	for (ba = 'a'; ba <= 'z'; ba++)
+		_putchar(ba);
+
+	_putchar('\n');
+}
