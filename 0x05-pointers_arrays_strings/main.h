78feec79a3077d260be330b6814161f09f494916
kwame7 <kofiagyeman540@gmail.com>
2022-12-19 20:52:37 -0800

    ama

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +12,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 int _atoi(char *s);
+int _putchar(char c);
 #endif
