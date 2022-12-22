680899c476c1b20dc41a6cedb5df91976e8757c7
kwame7 <kofiagyeman540@gmail.com>
2022-12-22 07:37:07 -0800

    ama

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,28 +1,20 @@
 #include "main.h"
-
 #include <stdio.h>
-
 /**
  * print_buffer - prints a buffer
  * @b: buffer.
  * @size: size of buffer.
  * Return: no return.
  */
-
 void print_buffer(char *b, int size)
-
 {
-
 int j, k, l;
 
 if (size <= 0)
-
 printf("\n");
 else
-
 {
 for (j = 0; j < size; j += 10)
-
 {
 printf("%.8x:", j);
 for (k = j; k < j + 10; k++)
@@ -39,7 +31,6 @@ for (l = j; l < j + 10; l++)
 {
 if (l >= size)
 break;
-
 if (*(b + l) < 32 || *(b + l) > 126)
 printf("%c", '.');
 else
